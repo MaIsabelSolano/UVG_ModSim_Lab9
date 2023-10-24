740298c62854302761e481f449a2db7726450c3e
DiegoPerdomo0805 <100246718+DiegoPerdomo0805@users.noreply.github.com>
2023-10-24 17:08:04 -0600

    cancha

--- Environment.py
+++ Environment.py
@@ -18,6 +18,10 @@ class Environment:
         self.x_goal = self.x_screen
         self.y_goal = self.y_screen/2
 
+
+        print("player: ", self.x_player, self.y_player)
+        print("ball: ", self.x_ball, self.y_ball)
+        print("goal: ", self.x_goal, self.y_goal)
         # setup de pygame
         pygame.init()
 
@@ -35,8 +39,8 @@ class Environment:
 
             # pos_goal = (self.x_goal, self.y_goal, 10, 10)
             # pygame.draw.rect(screen, "black", pos_goal, 5)
-            pos_goal = (self.x_goal, self.y_goal, 100, 5)
-            pygame.draw.rect(screen, "black", pos_goal, 6)
+            pos_goal = (self.x_goal-10, self.y_goal - 50, 10, 100)
+            pygame.draw.rect(screen, "white", pos_goal, 2)
             
             pos_ball = pygame.Vector2(self.x_ball, self.y_ball)
             pygame.draw.circle(screen, "red", pos_ball, 5)
