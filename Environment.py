7f38ccf5bf72441619c71a88eee156fe3ec2e478
Mxrk122 <60331995+Mxrk122@users.noreply.github.com>
2023-10-26 17:06:09 -0600

    graficos

--- Environment.py
+++ Environment.py
@@ -30,9 +30,6 @@ class Environment:
         # setup de pygame
         pygame.init()
 
-        # Configuración de Matplotlib para las funciones de pertenencia
-        plt.ion()  # Habilitar modo interactivo para actualizar gráficos en tiempo real
-
         screen = pygame.display.set_mode((self.x_screen, self.y_screen))
         clock = pygame.time.Clock()
         running = True 
@@ -65,9 +62,10 @@ class Environment:
                 self.diffuse()
             else:
                 running = False
-
+        self.generate_distance_graph()
+        self.generate_strength_graph()
         pygame.quit()
-        plt.show(block="False")
+        
 
 
     def diffuse(self):
@@ -79,7 +77,6 @@ class Environment:
             # patear la pelota en dirección a la portería
             self.kick(self.s)
             # Actualizar gráficos de funciones de pertenencia
-            
             pass
 
     
@@ -161,6 +158,7 @@ class Environment:
         dx, dy, l = self.ballDistance()
         s = self.defuzzify_strength(l)
         st = random.randint(8, 11) if s == "lejano" else random.randint(5, 7) if s == "medio" else random.randint(2, 4)
+        
         return st
 
         
@@ -170,3 +168,67 @@ class Environment:
 
         l = math.sqrt(dx**2 + dy**2)
         return dx, dy, l
+    
+    def generate_distance_graph(self):
+        # Configuración de Matplotlib para las funciones de pertenencia
+        plt.ion()
+        self.fig_distance, self.ax_distance = plt.subplots()
+        self.ax_distance.set_title('Función de Pertenencia para Distancia')
+        self.ax_distance.set_xlabel('Distancia')
+        self.ax_distance.set_ylabel('Pertenencia')
+
+        # Generar datos para el gráfico
+        x_vals = np.linspace(0, 150, 100)
+        y_vals = [self.membership_distance(d) for d in x_vals]
+
+        # Graficar la función de pertenencia
+        self.ax_distance.plot(x_vals, y_vals, label='Pertenencia')
+        self.ax_distance.legend()
+
+        self.ax_distance.set_yticks([0, 0.5, 1])
+        self.ax_distance.set_yticklabels(['Cercano', 'Medio', 'Lejano'])
+
+        # Mostrar el gráfico
+        plt.show(block="False")
+    
+    def membership_distance(self, distance):
+        if distance > 100:
+            return 0  # Pertenencia cercano
+        elif 30 < distance <= 100:
+            return (distance - 30) / 70  # Pertenencia medio
+        else:
+            return 1  # Pertenencia lejano
+    
+    def generate_strength_graph(self):
+        # Configuración de Matplotlib para las funciones de pertenencia
+        plt.ion()
+        self.fig_strength, self.ax_strength = plt.subplots()
+        self.ax_strength.set_title('Función de Pertenencia para Fuerza')
+        self.ax_strength.set_xlabel('Distancia')
+        self.ax_strength.set_ylabel('Pertenencia')
+
+        # Generar datos para el gráfico
+        x_vals = np.linspace(0, 250, 100)
+        y_vals = [self.membership_strength(d) for d in x_vals]
+
+        # Graficar la función de pertenencia
+        self.ax_strength.plot(x_vals, y_vals, label='Pertenencia')
+        self.ax_strength.legend()
+
+        # Añadir etiquetas personalizadas al eje y
+        self.ax_strength.set_yticks([0, 0.5, 1])
+        self.ax_strength.set_yticklabels(['Fuerte', 'Medio', 'Suave'])
+
+        # Añadir una descripción al gráfico
+        self.ax_strength.text(80, 0.8, 'Descripción: Esta es la función de pertenencia para la fuerza', fontsize=10, ha='center', va='center', bbox=dict(facecolor='white', alpha=0.5))
+
+        # Mostrar el gráfico
+        plt.show(block="False")
+
+    def membership_strength(self, distance):
+        if distance > 200:
+            return 1  # Pertenencia lejano
+        elif distance > 100:
+            return (distance - 100) / 100  # Pertenencia medio
+        else:
+            return 0  # Pertenencia cercano
